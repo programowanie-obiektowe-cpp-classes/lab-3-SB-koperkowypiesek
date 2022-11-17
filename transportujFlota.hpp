fa1fa255a135716331c17a80b5eeb9e58179d017
Szymon Bielenin <symonbielenin@gamil.com>
2022-11-17 16:53:12 +0100

    3rd

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -6,10 +6,11 @@ unsigned int transportujFlota(unsigned int towar)
 {
     unsigned int licznik_zaglowcow = 0;
     int          pozostały_towar   = towar;
+    Stocznia     stocznia_1{};
 
     while (pozostały_towar > 0) {
-        Stocznia stocznia_1{};
-        Statek*  statek_1 = stocznia_1();
+
+        Statek* statek_1 = stocznia_1();
         if (dynamic_cast< Zaglowiec* >(statek_1) != nullptr) {
             licznik_zaglowcow++;
         }
