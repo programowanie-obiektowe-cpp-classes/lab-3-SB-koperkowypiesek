2d3f435cfadaf561f9f121f0c9e1695f967dea23
Szymon Bielenin <symonbielenin@gamil.com>
2022-11-17 16:39:33 +0100

    1st

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int licznik_zaglowcow = 0;
+
+    while (towar > 0) {
+        Stocznia stocznia_1{};
+        Statek*  statek_1 = stocznia_1();
+        if (dynamic_cast< Zaglowiec* >(statek_1) != nullptr) {
+            licznik_zaglowcow++;
+        }
+        towar -= statek_1->transportuj();
+
+        delete statek_1;
+    }
+
+    return licznik_zaglowcow;
 }
