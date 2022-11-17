b27b7461c07a840a09e98e0820c2ef7c4bb90dee
Szymon Bielenin <symonbielenin@gamil.com>
2022-11-17 17:21:08 +0100

    reset

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,20 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int licznik_zaglowcow  = 0;
+    unsigned int przewieziony_towar = 0;
+    Stocznia     stocznia_1{};
+
+    while (przewieziony_towar < towar) {
+
+        Statek* statek_1 = stocznia_1();
+        if (dynamic_cast< Zaglowiec* >(statek_1) != nullptr) {
+            licznik_zaglowcow++;
+        }
+        przewieziony_towar += statek_1->transportuj();
+
+        delete statek_1;
+    }
+
+    return licznik_zaglowcow;
 }
